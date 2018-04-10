7c564ed7ccf1598e11f96969e94bcc742ab051c0
JNelson <nelson.jimmy@gmail.com>
2018-04-10 08:58:34 -0600

    removed validations for duration, end station, and end date

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,10 +1,7 @@
 # Defines Trip model
 class Trip < ApplicationRecord
-  validates :duration, presence: true
   validates :start_date, presence: true
   validates :start_station, presence: true
-  validates :end_date, presence: true
-  validates :end_station, presence: true
   validates :bike_id, presence: true
   validates :subscription_type, presence: true
   validates :zip_code, presence: true
