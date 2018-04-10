20482ce75100b1800bcb0a281d8b2b13bff5050e
JNelson <nelson.jimmy@gmail.com>
2018-04-09 19:02:34 -0600

    completed trip index complete with pagination

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,9 @@
+# Defines Trips Controller
+class TripsController < ApplicationController
+  def index
+    limit = 30
+    @pages = (Trip.all.size.to_f / limit).ceil
+    offset = params[:offset].to_i * limit ||= 0
+    @trips = Trip.all.offset(offset).limit(limit)
+  end
+end
---
+++ spec/features/user_visits_trips_index_sees_first_30_trips_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe 'User visits trip index' do
+  it 'should display first 30 trips and link to see more' do
+    trips = create_list(:trip, 35)
+
+    visit trips_path
+
+    expect(page).to have_content(trips.first.duration)
+    expect(page).to have_content(trips.first.start_date)
+    expect(page).to have_content(trips.first.start_station)
+    expect(page).to have_content(trips.first.end_date)
+    expect(page).to have_content(trips.first.end_station)
+    expect(page).to have_content(trips.first.bike_id)
+    expect(page).to have_content(trips.first.subscription_type)
+    expect(page).to have_content(trips.first.zip_code)
+
+    expect(page).to have_content(trips[29].bike_id)
+    expect(page).to_not have_content(trips[30].bike_id)
+  end
+end
+
+describe 'User visits trip index clicks to see more' do
+  it 'should display the next 30 trips' do
+    trips = create_list(:trip, 61)
+    visit trips_path
+    click_link '2'
+    save_and_open_page
+
+    expect(page).to_not have_content(trips[29].bike_id)
+    expect(page).to_not have_content(trips[60].bike_id)
+    expect(page).to have_content(trips[30].bike_id)
+  end
+end
