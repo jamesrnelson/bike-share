c6e44317be38a6bd4f480e543c9ffe1ba8ce8ac0
Evan Wheeler <anon0mys87@gmail.com>
2018-04-09 18:37:59 -0600

    Model, routes, and controller for Condition

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :condition do
     date '8/29/2013'
-    max_temperature_f { rand(75.0..90.0) }
-    mean_temperature_f { rand(65.0..75.0) }
-    min_temperature_f { rand(50.0..65.0) }
+    max_temperature { rand(75.0..90.0) }
+    mean_temperature { rand(65.0..75.0) }
+    min_temperature { rand(50.0..65.0) }
     mean_humidity { rand(65.0..75.0) }
     mean_visibility 10.0
     mean_wind_speed { rand(5.0..23.0) }
