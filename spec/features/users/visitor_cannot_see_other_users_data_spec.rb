d7590cf8a8ebefc4133028f3c639f4fe649e5b91
hmesander <haleymesander@gmail.com>
2018-04-11 11:13:15 -0600

    Starts writing test for user story

---
+++ spec/features/users/visitor_cannot_see_other_users_data_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+describe 'Visitor' do
+  it 'cannot view another users cart or admin data' do
+    
--- spec/features/visitors/visitor_cannot_see_other_user_data_spec.rb
+++ spec/features/visitors/visitor_cannot_see_other_user_data_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 describe 'Visitor' do
   it 'cannot see other user data or admin features' do
-    
+
   end
 end
