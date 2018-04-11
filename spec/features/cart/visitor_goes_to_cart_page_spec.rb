d6e1124964a207a7dc9e300b2fd5b04cebd53f9b
jrambold <jrambold@gmail.com>
2018-04-11 12:24:35 -0600

    cart persists through create user, removed through logout

--- spec/features/cart/visitor_goes_to_cart_page_spec.rb
+++ spec/features/cart/visitor_goes_to_cart_page_spec.rb
@@ -22,7 +22,43 @@ describe 'As a visitor,' do
       expect(page).to have_content('two')
       expect(page).to have_content(20)
       expect(page).to have_content('Total: 30.0')
+    end
 
+    scenario 'I can create an account and still see the same cart' do
+      item1 = Item.create!(title: 'one', description: 'blah', price: 5.0)
+      item2 = Item.create!(title: 'two', description: 'asdf', price: 20.0)
+
+      visit bike_shop_path
+
+      click_on item1.title
+      click_on item1.title
+
+      click_on item2.title
+
+      visit new_user_path
+
+      fill_in "user[name]", with: "Bob"
+      fill_in "user[username]", with: "bobbyboy"
+      fill_in "user[password]", with: "password"
+
+      click_on "Create User"
+
+      visit cart_path
+
+      expect(page).to have_content('one')
+      expect(page).to have_content(5)
+      expect(page).to have_content(2)
+      expect(page).to have_content(10)
+      expect(page).to have_content('two')
+      expect(page).to have_content(20)
+      expect(page).to have_content('Total: 30.0')
+
+      save_and_open_page
+      click_on "Logout"
+
+      visit cart_path
+
+      expect(page).to_not have_content('one')
     end
   end
 end
