f5d45d87774b5305149b233e568c2c25169663d7
Evan Wheeler <anon0mys87@gmail.com>
2018-04-10 23:34:09 -0600

    Added validations for condition attributes

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,3 +1,11 @@
 class Condition < ApplicationRecord
-
+  validates_presence_of :date,
+                       :max_temperature,
+                       :min_temperature,
+                       :mean_temperature,
+                       :mean_humidity,
+                       :mean_visibility,
+                       :mean_wind_speed,
+                       :precipitation,
+                       :zip_code
 end
