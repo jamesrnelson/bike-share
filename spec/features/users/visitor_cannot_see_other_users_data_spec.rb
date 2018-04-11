246da8ef8cc709466bb4da50929e99bd2670d6c7
hmesander <haleymesander@gmail.com>
2018-04-11 14:44:06 -0600

    Adds test for visitor to not see another user cart information

--- spec/features/users/visitor_cannot_see_other_users_data_spec.rb
+++ spec/features/users/visitor_cannot_see_other_users_data_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 describe 'Visitor' do
-  it 'cannot view another users cart or admin data' do
-    
+  it 'cannot view another users cart' do
+    user = create(:user)
+    item1, item2, item3 = create_list(:item, 3)
+
+    visit root_path
+
+    click_on 'Login'
+
+    expect(current_path).to eq(login_path)
+    fill_in 'name', with: user.name
+    fill_in 'username', with: user.username
+    fill_in 'password', with: user.password
+
+    within('.form') do
+      click_on 'Login'
+    end
+
+    visit bike_shop_path
+
+    click_on item1.title
+    click_on item2.title
+    click_on 'Logout'
+
+    visit bike_shop_path
+
+    click_on item3.title
+
+    visit cart_path
+
+    expect(page).to_not have_content(item1.title)
+    expect(page).to_not have_content(item2.title)
+    expect(page).to have_content(item3.title)
+  end
+end
