5a2c7e1bd6d4c2c1b4641d961ee2ee54cb6b30da
hmesander <haleymesander@gmail.com>
2018-04-10 20:33:19 -0600

    Adds user log out test

--- spec/features/auth/registered_user_can_log_out_spec.rb
+++ spec/features/auth/registered_user_can_log_out_spec.rb
@@ -1,1 +1,15 @@
 require 'rails_helper'
+
+describe 'Registered user' do
+  it 'can log out' do
+    user = create(:user)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    click_on 'Logout'
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content('You have logged out')
+    expect(page).to have_content('Login')
+  end
+end
