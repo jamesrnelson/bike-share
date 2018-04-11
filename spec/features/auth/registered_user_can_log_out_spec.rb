ad244bbffb0bc66b0740c3b0bea2aaa99a7aad47
hmesander <haleymesander@gmail.com>
2018-04-11 10:39:07 -0600

    Corrects log out test to NOT use stubbing

--- spec/features/auth/registered_user_can_log_out_spec.rb
+++ spec/features/auth/registered_user_can_log_out_spec.rb
@@ -4,7 +4,20 @@ describe 'Registered user' do
   it 'can log out' do
     user = create(:user)
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    visit root_path
+
+    click_on "Login"
+
+    expect(current_path).to eq(login_path)
+    fill_in "name", with: user.name
+    fill_in "username", with: user.username
+    fill_in "password", with: user.password
+
+    within('.form') do
+      click_on "Login"
+    end
+
+    expect(current_path).to eq(dashboard_path)
 
     click_on 'Logout'
 
