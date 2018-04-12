19147f3005235232d67e7351d32ee2412543637a
hmesander <haleymesander@gmail.com>
2018-04-12 08:58:44 -0600

    Corrects error in trip factory

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -24,9 +24,6 @@ FactoryBot.define do
     association :start_station, factory: :station
     end_date '2013-08-29 10:31:00'
     association :end_station, factory: :station
-    start_station 'University and Emerson'
-    end_date '2013-08-29 10:31:00'
-    end_station 'California Ave Caltrain Station'
     sequence(:bike_id) { |n| 1000000 + n }
     subscription_type 'Subscriber'
     zip_code 94303
