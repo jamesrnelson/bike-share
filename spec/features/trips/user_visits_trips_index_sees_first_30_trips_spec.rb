d3015b497ce09f67fb452f5fc803cbf30c4df49c
Evan Wheeler <34108893+anon0mys@users.noreply.github.com>
2018-04-12 12:05:35 -0600

    Revert "Admin trips index"

--- spec/features/trips/user_visits_trips_index_sees_first_30_trips_spec.rb
+++ spec/features/trips/user_visits_trips_index_sees_first_30_trips_spec.rb
@@ -1,78 +1,34 @@
 require 'rails_helper'
 
-feature 'User visits trip index' do
-  context 'as a visitor' do
-    it 'should see the first 30 trips and link to see more' do
-      trips = create_list(:trip, 35)
+describe 'User visits trip index' do
+  it 'should display first 30 trips and link to see more' do
+    trips = create_list(:trip, 35)
 
-      visit trips_path
+    visit trips_path
 
-      expect(page).to have_content(trips.first.duration)
-      expect(page).to have_content(trips.first.start_date)
-      expect(page).to have_content(trips.first.start_station.name)
-      expect(page).to have_content(trips.first.end_date)
-      expect(page).to have_content(trips.first.end_station.name)
-      expect(page).to have_content(trips.first.bike_id)
-      expect(page).to have_content(trips.first.subscription_type)
-      expect(page).to have_content(trips.first.zip_code)
+    expect(page).to have_content(trips.first.duration)
+    expect(page).to have_content(trips.first.start_date)
+    expect(page).to have_content(trips.first.start_station.name)
+    expect(page).to have_content(trips.first.end_date)
+    expect(page).to have_content(trips.first.end_station.name)
+    expect(page).to have_content(trips.first.bike_id)
+    expect(page).to have_content(trips.first.subscription_type)
+    expect(page).to have_content(trips.first.zip_code)
 
-      expect(page).to have_content(trips[29].bike_id)
-      expect(page).to_not have_content(trips[30].bike_id)
-
-      expect(page).to_not have_content('Edit')
-    end
-
-    it 'clicking the page 2 button should display the next 30 trips' do
-      trips = create_list(:trip, 61)
-
-      visit trips_path
-      click_link '2'
-
-      expect(page).to_not have_content(trips[29].bike_id)
-      expect(page).to_not have_content(trips[60].bike_id)
-      expect(page).to have_content(trips[30].bike_id)
-    end
+    expect(page).to have_content(trips[29].bike_id)
+    expect(page).to_not have_content(trips[30].bike_id)
   end
+end
 
-  context 'as an admin' do
-    scenario 'should see the first 30 trips' do
-      admin = create(:admin)
-      trips = create_list(:trip, 35)
+describe 'User visits trip index clicks to see more' do
+  it 'should display the next 30 trips' do
+    trips = create_list(:trip, 61)
 
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    visit trips_path
+    click_link '2'
 
-      visit trips_path
-
-      expect(page).to have_content(trips.first.duration)
-      expect(page).to have_content(trips.first.start_date)
-      expect(page).to have_content(trips.first.start_station.name)
-      expect(page).to have_content(trips.first.end_date)
-      expect(page).to have_content(trips.first.end_station.name)
-      expect(page).to have_content(trips.first.bike_id)
-      expect(page).to have_content(trips.first.subscription_type)
-      expect(page).to have_content(trips.first.zip_code)
-
-      expect(page).to have_content(trips[29].bike_id)
-      expect(page).to_not have_content(trips[30].bike_id)
-    end
-
-    scenario 'should see buttons to edit and delete a trip' do
-      admin = create(:admin)
-      trips = create_list(:trip, 35)
-
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-
-      visit trips_path
-
-      within 'tr:nth-child(2)' do
-        expect(page).to have_button 'Edit'
-        expect(page).to have_button 'Delete'
-      end
-
-      within 'tr:last-child' do
-        expect(page).to have_button 'Edit'
-        expect(page).to have_button 'Delete'
-      end
-    end
+    expect(page).to_not have_content(trips[29].bike_id)
+    expect(page).to_not have_content(trips[60].bike_id)
+    expect(page).to have_content(trips[30].bike_id)
   end
 end
