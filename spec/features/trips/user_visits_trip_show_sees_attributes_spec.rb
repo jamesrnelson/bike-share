f5391d031ae1bdda4681e1b581d4507665045db2
Evan Wheeler <anon0mys87@gmail.com>
2018-04-12 22:33:23 -0600

    Added edit and delete buttons to admin trips show

--- spec/features/trips/user_visits_trip_show_sees_attributes_spec.rb
+++ spec/features/trips/user_visits_trip_show_sees_attributes_spec.rb
@@ -1,18 +1,73 @@
 require 'rails_helper'
 
-describe 'User visits trip show page' do
-  it 'should see all attributes for that trip' do
-    trip = create(:trip)
+describe 'User visits the trip show page' do
+  context 'as a visitor' do
+    it 'they should see all attributes for that trip' do
+      trip = create(:trip)
 
-    visit trip_path(trip)
+      visit trip_path(trip)
 
-    expect(page).to have_content(trip.duration)
-    expect(page).to have_content(trip.start_date)
-    expect(page).to have_content(trip.start_station.name)
-    expect(page).to have_content(trip.end_date)
-    expect(page).to have_content(trip.end_station.name)
-    expect(page).to have_content(trip.bike_id)
-    expect(page).to have_content(trip.subscription_type)
-    expect(page).to have_content(trip.zip_code)
+      expect(page).to have_content(trip.duration)
+      expect(page).to have_content(trip.start_date)
+      expect(page).to have_content(trip.start_station.name)
+      expect(page).to have_content(trip.end_date)
+      expect(page).to have_content(trip.end_station.name)
+      expect(page).to have_content(trip.bike_id)
+      expect(page).to have_content(trip.subscription_type)
+      expect(page).to have_content(trip.zip_code)
+    end
+  end
+
+  context 'as an admin' do
+    scenario 'they should see all attributes for that trip' do
+      trip = create(:trip)
+      admin = create(:admin)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit trip_path(trip)
+
+      expect(page).to have_content(trip.duration)
+      expect(page).to have_content(trip.start_date)
+      expect(page).to have_content(trip.start_station.name)
+      expect(page).to have_content(trip.end_date)
+      expect(page).to have_content(trip.end_station.name)
+      expect(page).to have_content(trip.bike_id)
+      expect(page).to have_content(trip.subscription_type)
+      expect(page).to have_content(trip.zip_code)
+    end
+
+    scenario 'they should see edit button that links to edit trip page' do
+      trip = create(:trip)
+      admin = create(:admin)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit trip_path(trip)
+
+      expect(page).to have_button('Edit')
+
+      click_on 'Edit'
+
+      expect(current_path).to eq(edit_admin_trip_path(trip))
+    end
+
+    scenario 'they should see delete button that deletes the trip' do
+      trips = create_list(:trip, 35)
+      admin = create(:admin)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit trip_path(trips.first)
+
+      expect(page).to have_button('Delete')
+
+      click_on 'Delete'
+
+      expect(current_path).to eq(trips_path)
+      within 'tr:nth-child(2) td:first-child' do
+        expect(page).to_not have_content(trips.first.id)
+      end
+    end
   end
 end
