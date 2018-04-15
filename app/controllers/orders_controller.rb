fde5c256a7ede3c40eed739b203e72a163929504
hmesander <haleymesander@gmail.com>
2018-04-15 14:27:42 -0600

    Adds ability to update order status from the admin dashboard

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,6 +13,15 @@ class OrdersController < ApplicationController
     redirect_to dashboard_path
   end
 
+  def update
+    order = Order.find(params[:id])
+    order.update_attribute(:status, 3) if params[:status] == 'cancelled'
+    order.update_attribute(:status, 2) if params[:status] == 'paid'
+
+    flash.notice = "Order \##{order.id} was updated!"
+    redirect_to admin_dashboard_path
+  end
+
   private
     def order_params
       params.require(:order).permit(:fullname, :address)
--- spec/features/orders/admin_can_see_orders_dashboard_spec.rb
+++ spec/features/orders/admin_can_see_orders_dashboard_spec.rb
@@ -8,11 +8,7 @@ describe 'Admin' do
       order2 = user.orders.create!(fullname: 'Peter Smith', address: '400 Spencer St., Longmont, CO 80501', status: 1)
       order3 = user.orders.create!(fullname: 'Bob Ross', address: '1000 Colors St., Magical Land, CO 1000', status: 2)
       order4 = user.orders.create!(fullname: 'Another Name', address: '600 Colors St., Magical Land, CO 1000', status: 3)
-      item1, item2, item3 = create_list(:item, 3)
-      order1.order_items.create!(item_id: item1.id, quantity: 3)
-      order2.order_items.create!(item_id: item2.id, quantity: 1)
-      order3.order_items.create!(item_id: item3.id, quantity: 2)
-      order4.order_items.create!(item_id: item3.id, quantity: 2)
+
       admin = create(:admin)
 
       visit root_path
@@ -46,11 +42,7 @@ describe 'Admin' do
       order2 = user.orders.create!(fullname: 'Peter Smith', address: '400 Spencer St., Longmont, CO 80501', status: 1)
       order3 = user.orders.create!(fullname: 'Bob Ross', address: '1000 Colors St., Magical Land, CO 1000', status: 2)
       order4 = user.orders.create!(fullname: 'Another Name', address: '600 Colors St., Magical Land, CO 1000', status: 3)
-      item1, item2, item3 = create_list(:item, 3)
-      order1.order_items.create!(item_id: item1.id, quantity: 3)
-      order2.order_items.create!(item_id: item2.id, quantity: 1)
-      order3.order_items.create!(item_id: item3.id, quantity: 2)
-      order4.order_items.create!(item_id: item3.id, quantity: 2)
+
       admin = create(:admin)
 
       visit root_path
@@ -83,6 +75,70 @@ describe 'Admin' do
       click_on 'Total Cancelled: 1'
       expect(page).to have_content(order4.fullname)
       expect(page).to_not have_content(order2.fullname)
+    end
+
+    it 'they see cancel and mark as paid links' do
+      user = create(:user)
+      order1 = user.orders.create!(fullname: 'Haley Mesander', address: '936 Spencer St., Longmont, CO 80501')
+      order2 = user.orders.create!(fullname: 'Peter Smith', address: '400 Spencer St., Longmont, CO 80501', status: 1)
+      order3 = user.orders.create!(fullname: 'Bob Ross', address: '1000 Colors St., Magical Land, CO 1000', status: 2)
+      order4 = user.orders.create!(fullname: 'Another Name', address: '600 Colors St., Magical Land, CO 1000', status: 3)
+
+      admin = create(:admin)
+
+      visit root_path
+
+      click_on 'Login'
+
+      expect(current_path).to eq(login_path)
+      fill_in 'name', with: admin.name
+      fill_in 'username', with: admin.username
+      fill_in 'password', with: admin.password
+
+      within('.form') do
+        click_on 'Login'
+      end
+
+      visit admin_dashboard_path
+
+      expect(page).to have_link("Cancel Order \##{order1.id}")
+      expect(page).to have_link("Cancel Order \##{order1.id}")
+      expect(page).to_not have_link("Cancel Order \##{order4.id}")
+
+      expect(page).to have_link("Mark Order \##{order1.id} as Paid")
+      expect(page).to_not have_link("Mark Order \##{order2.id} as Paid")
+      expect(page).to_not have_link("Mark Order \##{order4.id} as Paid")
+    end
+
+    it 'they can cancel orders or mark orders as paid' do
+      user = create(:user)
+      order1 = user.orders.create!(fullname: 'Haley Mesander', address: '936 Spencer St., Longmont, CO 80501')
+      order2 = user.orders.create!(fullname: 'Peter Smith', address: '400 Spencer St., Longmont, CO 80501')
+
+      admin = create(:admin)
+
+      visit root_path
+
+      click_on 'Login'
+
+      expect(current_path).to eq(login_path)
+      fill_in 'name', with: admin.name
+      fill_in 'username', with: admin.username
+      fill_in 'password', with: admin.password
+
+      within('.form') do
+        click_on 'Login'
+      end
+
+      visit admin_dashboard_path
+
+      click_on "Cancel Order \##{order1.id}"
+
+      expect(page).to have_content('cancelled')
+
+      click_on "Mark Order \##{order2.id} as Paid"
+
+      expect(page).to have_content('paid')
     end
   end
 end
