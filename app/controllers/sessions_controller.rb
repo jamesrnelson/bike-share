4fcabf7252142af19da0759bb4275911c16adc56
hmesander <haleymesander@gmail.com>
2018-04-15 18:02:01 -0600

    Redirects admin to admin dashboard at login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
+      redirect_to admin_dashboard_path and return if current_admin?
       redirect_to dashboard_path
     else
       flash[:Error] = 'Incorrect Username or Password'
