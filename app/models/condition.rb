729d5a4b3835e8161d6752a06281c32187394c87
JNelson <nelson.jimmy@gmail.com>
2018-04-16 13:49:57 -0600

    created average rides method in condition model

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -24,4 +24,29 @@ class Condition < ApplicationRecord
     .order('trip_count ASC')
     .first
   end
+
+  def self.most_rides(info_hash)
+    select('conditions.*, count(trips.id) AS trip_count')
+    .joins('INNER JOIN trips ON conditions.date = trips.start_date')
+    .group('conditions.id')
+    .where(info_hash).order('trip_count DESC')
+    .first.trip_count
+  end
+
+  def self.least_rides(info_hash)
+    select('conditions.*, count(trips.id) AS trip_count')
+    .joins('INNER JOIN trips ON conditions.date = trips.start_date')
+    .group('conditions.id')
+    .where(info_hash)
+    .order("trip_count ASC")
+    .first.trip_count
+  end
+
+  def self.average_rides(info_hash)
+    trips = joins('INNER JOIN trips ON conditions.date = trips.start_date')
+            .group('trips.start_date')
+            .where(info_hash)
+            .count.values
+    trips.sum.to_f / trips.length
+  end
 end
