28b337e340d82e7f9d5bb223f90c6db739a6553c
Evan Wheeler <anon0mys87@gmail.com>
2018-04-17 15:01:46 -0600

    Removed zip code validation

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,7 +4,6 @@ class Trip < ApplicationRecord
   validates :start_station, presence: true
   validates :bike_id, presence: true
   validates :subscription_type, presence: true
-  validates :zip_code, presence: true
 
   belongs_to :start_station, :class_name => 'Station'
   belongs_to :end_station, :class_name => 'Station'
