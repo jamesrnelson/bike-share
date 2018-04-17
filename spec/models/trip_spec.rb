d8e98b4d3d977897cbb2db3cf960357b04d5d9c6
Evan Wheeler <anon0mys87@gmail.com>
2018-04-17 15:12:46 -0600

    Test fixes after rebase

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -6,7 +6,6 @@ describe Trip, type: :model do
     it { should validate_presence_of(:start_station) }
     it { should validate_presence_of(:bike_id) }
     it { should validate_presence_of(:subscription_type) }
-    it { should validate_presence_of(:zip_code) }
   end
 
   describe 'class methods' do
